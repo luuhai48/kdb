1504f7b0ff5cf75af6a96b78171cf360eb2fdbaf
luuhai48 <luuhai.hn48@gmail.com>
2023-09-03 22:19:09 +0700

    Add tailwindcss

--- src/renderer/routes/home.js
+++ src/renderer/routes/home.js
@@ -2,7 +2,9 @@ import m from 'mithril';
 
 const Home = {
   view: function () {
-    return m('main', [m('h1', { class: 'title' }, 'KDB')]);
+    return m('main', [
+      m('h1', { class: 'title text-center text-2xl font-bold' }, 'KDB'),
+    ]);
   },
 };
 
